565dff864a23f620dc9045af7761c67b4919c8b2
guyu0420 <guyuguyu111@gmail.com>
2018-04-02 09:39:38 +0800

    Production

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
  var path = require('path');
  var webpack = require('webpack');
-
+const htmlWebpackPlugin = require('html-webpack-plugin');
  module.exports = {
      entry: './js/app.js',
      output: {
-         path: path.resolve(__dirname, 'build'),
+         //path: path.resolve(__dirname, 'build'),
+         path:path.resolve('./dist'),
          filename: 'main.bundle.js'
      },
      module: {
@@ -22,6 +23,12 @@
              }
          ]
      },
+     plugins:[
+        new htmlWebpackPlugin({
+            template:'./index.html', //template参照文件来移动文件
+            //生成路径：其可以获取output中的path属性
+        })
+    ],
      stats: {
          colors: true
      },
